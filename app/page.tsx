3af53b31897d6b362981aadd55309185da488339
aigle-levant <aiglelevant@gmail.com>
2026-03-10 22:40:08 +0530

    Final edit

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
-            Macrosatic powered by{" "}
+            Macrosatic, powered by{" "}
             <a
               href="https://supabase.com"
               target="_blank"
